97283134d9f5915e8e3803aa6da9f04fa5abb89f
LaiZiSen <samlain060504>
2021-10-11 21:05:54 +0800

    Crated flutter project and added licence

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Account Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Account Manager'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Container(
+            child: Container(
+          child: Text("Temperaly"),
+        )));
+  }
+}
