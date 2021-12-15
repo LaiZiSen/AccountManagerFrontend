cfb717976c58979018173431369834a3efcf8a3d
LaiZiSen <samlain060504>
2021-12-15 21:19:43 +0800

    Modified text field and added enter function

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,16 +5,23 @@ AppBar appBar = AppBar(
   title: Text("Login"),
 );
 
-Widget TextField_(String hintText, TextEditingController controller) {
+Widget TextField_(context, String hintText, TextEditingController controller,
+    FocusNode focusNode, FocusNode nextField) {
   return Container(
     padding: EdgeInsets.symmetric(vertical: 8, horizontal: 40),
     child: TextField(
+      focusNode: focusNode,
       controller: controller,
       textAlign: TextAlign.left,
       decoration: InputDecoration(
         hintText: hintText,
         border: OutlineInputBorder(),
       ),
+      onSubmitted: (_) {
+        if (nextField != null) {
+          FocusScope.of(context).requestFocus(nextField);
+        }
+      },
     ),
   );
 }
@@ -27,6 +34,7 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
+  final FocusNode _passwordFocusNode = FocusNode();
   var test = "test";
 
   @override
@@ -41,6 +49,16 @@ class _LoginScreenState extends State<LoginScreen> {
     _email.dispose();
     _password.dispose();
     super.dispose();
+  }
+
+  void enter() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: Text("Email: ${_email.text} Password: ${_password.text}"),
+          );
+        });
   }
 
   @override
@@ -71,21 +89,15 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ),
             ),
-            TextField_('Email', _email),
-            TextField_('Password', _password),
+            TextField_(context, 'Email', _email, null, _passwordFocusNode),
+            TextField_(
+                context, 'Password', _password, _passwordFocusNode, null),
             Container(
               padding: EdgeInsets.only(),
               alignment: Alignment.center,
               child: TextButton(
                 onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          content: Text(
-                              "Email: ${_email.text} Password: ${_password.text}"),
-                        );
-                      });
+                  enter();
                 },
                 child: Text("Enter"),
               ),
